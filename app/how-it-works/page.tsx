cc6e1d26f74ddde39faff130c1ae96372a033135
boopathyganesh <boopathyganesh95@gmail.com>
2024-03-15 23:37:22 +0530

    Infographics Test Commit

--- app/how-it-works/page.tsx
+++ app/how-it-works/page.tsx
@@ -11,13 +11,7 @@ export const metadata = {
   export default function HowItWorks() {
     return (
       <>    
-          <section className='max-w-full mt-48 px-2 md:px-4 lg:px-8 lg:mx-4 xl:px-12 xl:mx-8'>
-            <div className='px-2'>
-                <div className='font-extrabold text-2xl md:text-4xl lg:text-7xl text-gold-600 text-center'>"WE INNOVATE FOR PEOPLE TO SAVE GOLD"</div>
-                <div className='font-normal text-sm md:text-xl lg:text-2xl text-center mt-4'>India's 1st omni channel gold store co-owned by customers & workforce!</div>
-            </div>
-
-
+          <section className='max-w-full mt-28 px-2 md:px-4 lg:px-8 lg:mx-4 xl:px-12 xl:mx-8'>
             <div className='pt-4 lg:pt-16'>
                 <Infographics />
             </div>
--- components/infographics/infographics.tsx
+++ components/infographics/infographics.tsx
@@ -1,85 +1,53 @@
 import '@/components/infographics/infographics.css'
+import Pointer from '@/public/images/svgs/pointer.svg'
+import Image from 'next/image'
+import User from '@/public/images/svgs/add-user.svg'
+
 
 const Infographics = () => {
 
     // https://codepen.io/siiron/pen/zKGrvE
 
-  return (
-        <div className="container">
-            <ol className="step-list">
-                <li className="step-list__item">
-                    <div className="step-list__item__inner">
-                        <div className="content">
-                            <div className="body">
-                                <h2>Lorem ipsum dolor sit amet</h2>
-                                <p>Consectetur adipisicing elit. Reprehenderit perspiciatis.</p>
-                            </div>
+    return (
+        <section className='flex items-center justify-center'>
 
-                            <div className="icon">
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/13060/check-circle.svg" alt="Check" />
-                            </div>                
+            <div className='w-full max-w-sm relative'>
+                <div className='flex flex-wrap items-center bg-white py-4 px-5 rounded-xl pb-20 relative overflow-x-visible'>
+                    <div className='font-bold text-3xl text-gold-600'>How It Works</div>
+                    <div className='font-normal text-black-700 text-sm pt-2'>Value1 allows you to tailor your investing experience to fit your individual needs.</div>
+                    <div className='font-normal text-black-700 text-sm pt-2'>Value1 customers have the unique option of buying immediately, pre-funding, and the ability to transact 24/7.</div>
+                    <div className='absolute z-10 bottom-4 left-5 w-10'><Image src={Pointer} height={100} width={100} alt='Pointer' /></div>
+                    <div className='absolute z-10 bg-transparent w-20 bottom-[34px] left-16 border-2 border-gold-500 border-dashed rounded-full'></div>
+                    <div className='absolute z-10 bg-transparent h-20 -bottom-[50px] left-[140px] border-2 border-gold-500 border-dashed rounded-full'></div>
+                </div>
+                
+                <div className='infographic-section mt-16 relative flex justify-center'>
+                    <div className='p-1 steps'>
+                        <div className='absolute z-10 -top-2 left-[122px] w-10'><Image src={Pointer} height={100} width={100} alt='Pointer' /></div>
+                        <div className='relative step-container left-[74px] top-10 z-0 flex flex-col items-center text-gold-500 px-1 py-1 rounded-xl w-max max-w-xs'>
+                            <div>
+                                <Image src={User} alt='Add User' className='w-16 px-1' />
+                            </div>
+                            <div className='font-bold text-sm'>Create An Account</div>
+                            <div className='font-light text-xs w-28'>This is free and only takes a few minutes.</div>
                         </div>
                     </div>
-                </li>
-                <li className="step-list__item">
-                    <div className="step-list__item__inner">
-                        <div className="content">
-                            <div className="body">
-                                <h2>Impedit ducimus saepe assumenda</h2>
-                                <p>Sapiente beatae? Quo maiores consequatur, eveniet autem eos quia molestias perferendis.</p>
+                    <div className='p-1 steps'>
+                        <div className='absolute z-10 -top-2 left-[122px] w-10'><Image src={Pointer} height={100} width={100} alt='Pointer' /></div>
+                        <div className='relative step-container left-[74px] top-10 z-0 flex flex-col items-center text-gold-500 px-1 py-1 rounded-xl w-max max-w-xs'>
+                            <div>
+                                <Image src={User} alt='Add User' className='w-16 px-1' />
                             </div>
-
-                            <div className="icon">
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/13060/mail_outline_copy.svg" alt="Check" />
-                            </div>                
+                            <div className='font-bold text-sm'>Create An Account</div>
+                            <div className='font-light text-xs w-28'>This is free and only takes a few minutes.</div>
                         </div>
                     </div>
-                </li>
-                <li className="step-list__item">
-                    <div className="step-list__item__inner">
-                        <div className="content">
-                            <div className="body">
-                                <h2>Repellendus</h2>
-                                <p> Asperiores eum, accusantium harum, aperiam labore assumenda quisquam tempore magnam enim iusto voluptatum aspernatur dicta saepe possimus nobis molestiae quas sapiente.</p>
-                            </div>
+                </div>
+            </div>
 
-                            <div className="icon">
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/13060/graph.svg" alt="Check" />
-                            </div>                
-                        </div>
-                    </div>
-                </li>
-                <li className="step-list__item">
-                    <div className="step-list__item__inner">
-                        <div className="content">
-                            <div className="body">
-                                <h2>Quaerat</h2>
-                                <p> Iusto quod incidunt vel quidem fuga quos laudantium dignissimos eos, tempore sequi quis praesentium.</p>
-                            </div>
 
-                            <div className="icon">
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/13060/calendar.svg" alt="Check" />
-                            </div>                
-                        </div>
-                    </div>
-                </li>
-                <li className="step-list__item">
-                    <div className="step-list__item__inner">
-                        <div className="content">
-                            <div className="body">
-                                <h2>Voluptatum alias hic</h2>
-                                <p>Officiis excepturi atque velit asperiores cum perferendis, repellendus facilis voluptatibus quas! Consequuntur.</p>
-                            </div>
-
-                            <div className="icon">
-                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/13060/heart.svg" alt="Check" />
-                            </div>                
-                        </div>
-                    </div>
-                </li>
-            </ol>
-        </div>
-  )
+        </section>
+    )
 }
 
 export default Infographics
